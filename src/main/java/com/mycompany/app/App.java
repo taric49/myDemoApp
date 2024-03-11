c97b9216eaa3717c32f4b8f8b19906010bb48432
taric49 <tderici@edu.edu.tr>
2024-03-11 09:20:52 +0300

    trying to trigger travis i hope this time works

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,7 +3,6 @@ package com.mycompany.app;
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
